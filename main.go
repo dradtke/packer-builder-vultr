82842d6e750f61678a0a682e41038eccb7c190fa
Damien Radtke <damienradtke@gmail.com>
2018-09-28 11:43:26 -0500

    Simplify main method

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
+	"github.com/hashicorp/packer/packer/plugin"
+
 	"github.com/dradtke/packer-builder-vultr/vultr"
-	"github.com/mitchellh/packer/packer/plugin"
 )
 
 func main() {
-	server, err := plugin.Server()
-	if err != nil {
-		panic(err)
-	}
-	server.RegisterBuilder(new(vultr.Builder))
-	server.Serve()
+	plugin.RegisterBuilder(new(vultr.Builder))
 }
